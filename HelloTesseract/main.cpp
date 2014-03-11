92e456320987d82f90a1ba128ebca848b3affeae
AronHuang <aronexplorer@gmail.com>
2014-03-11 00:25:39 -0700

    init commit

--- HelloTesseract/main.cpp
+++ HelloTesseract/main.cpp
@@ -8,11 +8,32 @@
 
 #include <iostream>
 
+#include <tesseract/baseapi.h>
+#include <leptonica/allheaders.h>
+
 int main(int argc, const char * argv[])
 {
 
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+    char *outText;
+    
+    tesseract::TessBaseAPI *api = new tesseract::TessBaseAPI();
+    // Initialize tesseract-ocr with English, without specifying tessdata path
+    if (api->Init(NULL, "eng")) {
+        fprintf(stderr, "Could not initialize tesseract.\n");
+        exit(1);
+    }
+    
+    // Open input image with leptonica library
+    Pix *image = pixRead("RunningTesseract.png");
+    api->SetImage(image);
+    // Get OCR result
+    outText = api->GetUTF8Text();
+    printf("OCR output:\n%s", outText);
+    
+    // Destroy used object and release memory
+    api->End();
+    delete [] outText;
+    pixDestroy(&image);
+
 }
 

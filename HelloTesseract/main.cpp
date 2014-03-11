0581ec931e73265e378eea9a744a3d10996edf12
AronHuang <aronexplorer@gmail.com>
2014-03-10 23:34:59 -0700

    Initial Commit

---
+++ HelloTesseract/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  HelloTesseract
+//
+//  Created by Haoyu Huang on 3/10/14.
+//  Copyright (c) 2014 HaoyuHuang. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
